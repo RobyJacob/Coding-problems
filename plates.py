cdff58f845abc1cc0b9c7ce2b98f73be16d42afa
robyjacob <robyjacob1998@gmail.com>
2020-05-20 05:11:07 +0530

    add kickstart 2020 round A 2nd problem

---
+++ plates.py
@@ -0,0 +1,37 @@
+def beauty(idx, taken):
+    if taken == P:
+        return 0
+    if idx > N or taken > P:
+        return -1e9
+
+    if cache[idx][taken] != -1:
+        return cache[idx][taken]
+
+    ans = -1e9
+    for i in range(K+1):
+        ans = max(ans, pref[idx][i] + beauty(idx + 1, taken + i))
+    cache[idx][taken] = ans
+
+    return cache[idx][taken]
+
+t = int(input())
+
+for tc in range(t):
+    N, K, P = list(map(int, input().split()))
+
+    stacks = [[0 for _ in range(K+1)] for _ in range(N+1)]
+
+    for i in range(1, N+1):
+        stacks[i][1:] = list(map(int, input().split()))
+
+    # print(stacks)
+    pref = [[0 for _ in range(K+1)] for _ in range(N+1)]
+    cache = [[-1 for _ in range((N+1)*K)] for _ in range(N+1)]
+
+    for i in range(1, N+1):
+        for j in range(1, K+1):
+            pref[i][j] = pref[i][j-1] + stacks[i][j]
+
+    ans = beauty(1, 0)
+
+    print("Case #{}: {}".format(tc+1, ans))
